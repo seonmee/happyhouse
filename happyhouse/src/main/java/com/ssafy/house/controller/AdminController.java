ff594d65395482b534371d29bd4e78733164d612
hyeonseonmi <hyeonseonmi@localhost>
2021-05-23 21:54:19 +0900

    Update 관리자 by 김현홍

--- happyhouse/src/main/java/com/ssafy/house/controller/AdminController.java
+++ happyhouse/src/main/java/com/ssafy/house/controller/AdminController.java
@@ -15,12 +15,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ssafy.house.dto.BoardDto;
 import com.ssafy.house.dto.MemberDto;
+import com.ssafy.house.dto.SearchCondition;
+import com.ssafy.house.help.NumberResult;
 import com.ssafy.house.service.UserService;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 //@RestController
 //@RequestMapping("/admin")
@@ -36,49 +41,87 @@ public class AdminController {
 	@Autowired
 	private UserService userService;
 	
-	@GetMapping(value = "/admin/user")
-	public ResponseEntity<List<MemberDto>> userList() {
-		logger.debug("사용자 목록 표시.........");
-		
-		List<MemberDto> list = userService.userList();
-		if(list != null && !list.isEmpty()) {
-			return new ResponseEntity<List<MemberDto>>(list, HttpStatus.OK);
-		} else {
+	@ApiOperation(value = "모든 유저의 정보를 반환한다.", response = List.class)
+	@RequestMapping(value = "/admin/user", method = RequestMethod.GET)
+	public ResponseEntity<List<MemberDto>> boards(SearchCondition condition) throws Exception {
+		logger.info("1-------------findAllMember-----------------------------"+condition);
+		int start = condition.getCurrentPage() * condition.countPerPage - condition.countPerPage;
+		condition.setStart(start);
+		condition.setSpp(condition.countPerPage);
+		logger.info("2-------------find-----------------------------"+condition);
+		List<MemberDto> member = userService.userList(condition);
+		if (member.isEmpty()) {
 			return new ResponseEntity(HttpStatus.NO_CONTENT);
 		}
+		return new ResponseEntity<List<MemberDto>>(member, HttpStatus.OK);
 	}
 	
-	@PostMapping(value = "/admin/user")
-	public ResponseEntity<List<MemberDto>> userRegister(@RequestBody MemberDto memberDto) {
-		boolean total = userService.userRegister(memberDto);
-		if(total) {
-			List<MemberDto> list = userService.userList();
-			return new ResponseEntity<List<MemberDto>>(list, HttpStatus.OK);
-		} else
-			return new ResponseEntity(HttpStatus.NO_CONTENT);
+	@ApiOperation(value = "모든 유저의 개수 반환", response = NumberResult.class)
+	@RequestMapping(value = "/admin/total", method = RequestMethod.GET)
+	public ResponseEntity<NumberResult> totalBoard(SearchCondition condition) throws Exception {
+		logger.info("1-------------findAllBoard-----------------------------"+condition);
+		int total = userService.getTotalLength(condition);
+   		NumberResult nr=new NumberResult();
+   		nr.setCount(total);
+   		nr.setName("Member");
+   		nr.setState("succ");
+   		logger.info("5-------------test------------------"+total);
+   		if (total<=0) {
+   			nr.setCount(-1);
+   	   		nr.setName("nomember");
+   	   		nr.setState("fail");
+   			return new ResponseEntity<NumberResult>(nr, HttpStatus.OK);
+   		}
+   		return new ResponseEntity<NumberResult>(nr, HttpStatus.OK);
 	}
 	
-	@GetMapping(value = "/admin/user/{userid}")
-	public ResponseEntity<MemberDto> userInfo(@PathVariable("userid") String userid) {
-		logger.debug("파라미터 : {}", userid);
-		MemberDto memberDto = userService.userInfo(userid);
-		if(memberDto != null)
-			return new ResponseEntity<MemberDto>(memberDto, HttpStatus.OK);
-		else
-			return new ResponseEntity(HttpStatus.NO_CONTENT);
-	}
+//	@GetMapping(value = "/admin/user")
+//	public ResponseEntity<List<MemberDto>> userList() {
+//		logger.debug("사용자 목록 표시.........");
+//		
+//		List<MemberDto> list = userService.userList();
+//		if(list != null && !list.isEmpty()) {
+//			return new ResponseEntity<List<MemberDto>>(list, HttpStatus.OK);
+//		} else {
+//			return new ResponseEntity(HttpStatus.NO_CONTENT);
+//		}
+//	}
 	
-	@PutMapping(value = "/admin/user")
-	public ResponseEntity<List<MemberDto>> userModify(@RequestBody MemberDto memberDto) {
-		userService.userModify(memberDto);
-		List<MemberDto> list = userService.userList();
-		return new ResponseEntity<List<MemberDto>>(list, HttpStatus.OK);
-	}
+//	@PostMapping(value = "/admin/user")
+//	public ResponseEntity<List<MemberDto>> userRegister(@RequestBody MemberDto memberDto) {
+//		boolean total = userService.userRegister(memberDto);
+//		if(total) {
+//			List<MemberDto> list = userService.userList();
+//			return new ResponseEntity<List<MemberDto>>(list, HttpStatus.OK);
+//		} else
+//			return new ResponseEntity(HttpStatus.NO_CONTENT);
+//	}
+	
+//	@GetMapping(value = "/admin/user/{userid}")
+//	public ResponseEntity<MemberDto> userInfo(@PathVariable("userid") String userid) {
+//		logger.debug("파라미터 : {}", userid);
+//		MemberDto memberDto = userService.userInfo(userid);
+//		if(memberDto != null)
+//			return new ResponseEntity<MemberDto>(memberDto, HttpStatus.OK);
+//		else
+//			return new ResponseEntity(HttpStatus.NO_CONTENT);
+//	}
+	
+//	@PutMapping(value = "/admin/user")
+//	public ResponseEntity<List<MemberDto>> userModify(@RequestBody MemberDto memberDto) {
+//		userService.userModify(memberDto);
+//		List<MemberDto> list = userService.userList();
+//		return new ResponseEntity<List<MemberDto>>(list, HttpStatus.OK);
+//	}
 	
 	@DeleteMapping(value = "/admin/user/{userid}")
 	public ResponseEntity<List<MemberDto>> userDelete(@PathVariable("userid") String userid) {
 		userService.userDelete(userid);
-		List<MemberDto> list = userService.userList();
+		SearchCondition condition = new SearchCondition();
+		int start = condition.getCurrentPage() * condition.countPerPage - condition.countPerPage;
+		condition.setStart(start);
+		condition.setSpp(condition.countPerPage);
+		List<MemberDto> list = userService.userList(condition);
 		return new ResponseEntity<List<MemberDto>>(list, HttpStatus.OK);
 	}
 	
